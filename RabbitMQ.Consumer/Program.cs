ee11b8ca62c53d9467d42d9fbac30946a0f15a2b
Evhenii Tsyvinda <yevheniitsyvinda@gmail.com>
2022-07-19 15:37:56 +0300

    Add project files.

---
+++ RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,34 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Text.Json;
+
+const string _rabbitMQURL = "amqp://guest:guest@localhost:5672";
+const string _queueName = "demo-queue";
+
+
+var factory = new ConnectionFactory
+{
+    Uri = new Uri(_rabbitMQURL)
+};
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+channel.QueueDeclare(_queueName,
+    durable: true,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (sender, e) =>
+{
+    var body = e.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine(message);
+};
+
+channel.BasicConsume(_queueName,true,consumer);
+
+Console.ReadKey();
